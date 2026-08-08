c89be98b4073eb4f46cf2efd860d8e118619ed78
Luciano <orisluciano@gmail.com>
2026-08-08 11:50:22 -0300

    producto- servicio nuev producto

--- pos/src/main/resources/static/producto/servicio/ProductoServicio.js
+++ pos/src/main/resources/static/producto/servicio/ProductoServicio.js
@@ -15,6 +15,28 @@ class ProductoServicio {
         }
         return respuesta;
     }
+
+    async nuevo(producto) {
+        let errores = [];
+        if (Object.hasOwn(producto, "nombre")) {
+            errores.push("Falta el nombre");
+        }
+        if (Object.hasOwn(producto, "tipoProducto")) {
+            errores.push("Falta el tipo de producto");
+        }
+        let respuesta = null;
+        if (errores.length > 0) {
+            respuesta.errores = errores;
+        } else {
+            try {
+                let resBase = await fetch(this.api);
+                respuesta = await resBase.json();
+            } catch (error) {
+                respuesta = error;
+            }   
+        }
+        return respuesta;
+    }
 }
 
 export default ProductoServicio;
